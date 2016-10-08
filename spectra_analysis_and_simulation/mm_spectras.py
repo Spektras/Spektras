65f41d399dc799523d9e3c13d2a1655170f74841
Spektras <labaku@email.cz>
2016-10-09 00:12:19 +0200

    FIX: fit

--- spectra_analysis_and_simulation/mm_spectras.py
+++ spectra_analysis_and_simulation/mm_spectras.py
@@ -54,40 +54,12 @@ b = T_min/(np.log(T_min-I_lines*E*k_B))
 T1_1 = np.abs(T0_1*b)*scipy.misc.logsumexp(I_lines)
 T = (np.abs(np.gradient(np.gradient(T1_1)))+T0)**2/(2*T0)
 dT = np.gradient(T)
-v_e = 4.19*10**7*T**1/2 #cm/s
-v_Fe = 9.79*10**5*(9.2703e-026/m_p)**1/2*T**1/2 # mean q velocity of iron ion
+def f(s,t):
+	return [t*s[1],s[0]]
 s1_0 = 2*10**-7/7100/gamma(2*10**-7)
 s0_0 = -8*10**-7/7100/gamma(-8*10**-7)
 s0 = [s0_0,s1_0]
-def f(s,t):
-	return [t*s[1],s[0]]
 t = np.arange(0,7100,(7100/len(T)))
-s_e = (odeint(f,s0,t)[:,0]+odeint(f,s0,t)[:,1])*v_e
-s_Fe = (odeint(f,s0,t)[:,0]+odeint(f,s0,t)[:,1])*v_Fe
-fig0 = plt.figure()
-plt.plot(T,s_e)
-plt.xlabel('Temperature (K)')
-plt.ylabel('Fluctuation scope (cm)')
-fig0.savefig('Electron_fluctuation.png')
-fig1 = plt.figure()
-plt.plot(T,s_Fe)
-plt.xlabel('Temperature (K)')
-plt.ylabel('Fluctuation scope (cm)')
-fig1.savefig('Iron_fluctuation.png')
-lambda1_lines = lambda_lines*10**9
-np.savetxt('temperature.txt',T)
-fig2 = plt.figure()
-plt.plot(lambda_m_nm[index],T)
-plt.xlabel('Wavelength (nm)')
-plt.ylabel('Temperature (K)')
-plt.xlim(xmin=190)
-plt.xlim(xmax=900)
-fig2.savefig('Temperature_fit0.png')
-fig3 = plt.figure()
-plt.plot(T,I_lines)
-plt.ylabel('Intensity (a.u.)')
-plt.xlabel('Temperature (K)')
-fig3.savefig('Temperature_fit.png')
 a = k1**(k1-1)/factorial(k1)*1/3*(9/16*(epsilon*k_B*np.e)*np.pi**-2*np.exp(-dT/(4*np.pi)))**k1+1
 b = a*16/9*epsilon**3*k_B**2*np.pi**2*lambda_De**(6/5)*np.exp(dT/2.241)/(np.e**6*22.41**2)
 n = (np.exp(-np.log(b*10**12)/20)*np.exp((b*10**12)/20))+np.exp((b*10**12)/20)
@@ -257,3 +229,46 @@ np.savetxt('concentration.txt',concentration)
 np.savetxt('concetration_statistics.txt',concentration_sigma)
 np.savetxt('concentration_relative.txt',concentration_relative)
 np.savetxt('concentration_ratio.txt',concentration_ratio)
+def lorentz(x, mu, a, fwhm):
+        gamma = fwhm/2.
+        return ((1./np.pi) * ((0.5*gamma)/((x - mu)**2 + (0.5*gamma)**2))) * a
+
+I0 = ablation[:,1]
+I_fit = 1/lorentz(lambda_m_nm,I0,10,(max(I0)-min(I0)))-min(1/lorentz(lambda_m_nm,I0,10,(max(I0)-min(I0))))
+T0 = (wien/lambda_m-mean(wien/lambda_m)+np.abs(min(wien/lambda_m-mean(wien/lambda_m)))) - (np.abs(wien/lambda_m-mean(wien/lambda_m))+(max(np.abs(wien/lambda_m-mean(wien/lambda_m)))-np.abs(wien/lambda_m-mean(wien/lambda_m))))+np.abs(min((wien/lambda_m-mean(wien/lambda_m)+np.abs(min(wien/lambda_m-mean(wien/lambda_m)))) - (np.abs(wien/lambda_m-mean(wien/lambda_m))+(max(np.abs(wien/lambda_m-mean(wien/lambda_m)))-np.abs(wien/lambda_m-mean(wien/lambda_m))))))
+E = h*c/lambda_m[index]
+a = k1**(k1-1)/factorial(k1)*(1/((T_min*(E*k_B)**(T_min))-np.log(T_min)))**k1
+T0_1 = np.abs(np.log(1/(a+np.e)))
+b = T_min/(np.log(T_min-I_lines*E*k_B))
+T1_1 = np.abs(T0_1*b)*scipy.misc.logsumexp(I_lines)
+T = (np.sqrt(T1_1)+T0)**2/(2*max(T0))
+T[np.isinf(T)]=0
+dT = np.gradient(T)
+v_e = 4.19*10**7*T**1/2 #cm/s
+v_Fe = 9.79*10**5*(9.2703e-026/m_p)**1/2*T**1/2 # mean q velocity of iron ion
+s_e = (odeint(f,s0,t)[:,0]+odeint(f,s0,t)[:,1])*v_e
+s_Fe = (odeint(f,s0,t)[:,0]+odeint(f,s0,t)[:,1])*v_Fe
+fig0 = plt.figure()
+plt.plot(T,s_e)
+plt.xlabel('Temperature (K)')
+plt.ylabel('Fluctuation scope (cm)')
+fig0.savefig('Electron_fluctuation.png')
+fig1 = plt.figure()
+plt.plot(T,s_Fe)
+plt.xlabel('Temperature (K)')
+plt.ylabel('Fluctuation scope (cm)')
+fig1.savefig('Iron_fluctuation.png')
+lambda1_lines = lambda_lines*10**9
+np.savetxt('temperature.txt',T)
+fig2 = plt.figure()
+plt.plot(lambda_m_nm,T)
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Temperature (K)')
+plt.xlim(xmin=190)
+plt.xlim(xmax=900)
+fig2.savefig('Temperature_fit.png')
+fig3 = plt.figure()
+plt.plot(lambda_m_nm,I_fit)
+plt.ylabel('Intensity (a.u.)')
+plt.xlabel('Wavelength (nm)')
+fig3.savefig('_fit.png')
